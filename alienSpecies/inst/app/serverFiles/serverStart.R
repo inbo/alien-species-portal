41d6db3589185e8654ca9ea5daaf58c8c978d1fa
mvarewyck <machteld.varewyck@openanalytics.eu>
2022-06-28 15:34:09 +0200

    improve start page

--- alienSpecies/inst/app/serverFiles/serverStart.R
+++ alienSpecies/inst/app/serverFiles/serverStart.R
@@ -3,5 +3,11 @@
 observeEvent(input$start_navigate, {
     
       updateNavbarPage(session = session, inputId = "tabs", selected = input$start_navigate)
+      
+  })
+
+observeEvent(input$tabs, {
+    
+    updateRadioButtons(session = session, inputId = "start_navigate", selected = input$tabs)
     
   })
--- alienSpecies/inst/app/uiFiles/uiStart.R
+++ alienSpecies/inst/app/uiFiles/uiStart.R
@@ -6,9 +6,10 @@ tagList(
     align = "center", 
     tags$div(class = "noButton",
       radioButtons(inputId = "start_navigate", label = "", inline = TRUE,
-        choiceValues = list("Start", "Global Indicators", "Species Information", "Early Warning", "Management"),
+        choiceValues = list("Start", "Global Indicators", "Species Information", 
+          "Early Warning?", "Management?"),
         choiceNames = list(
-          HTML("<div class='fotoTitel'>Start</div><div id='exampleFoto'></div>"),
+          "",
           HTML("<div class='fotoTitel'>Indicators</div><div id='exampleFoto'></div>"),
           HTML("<div class='fotoTitel'>Species</div><div id='exampleFoto'></div>"),
           HTML("<div class='fotoTitel'>Early Warning</div><div id='exampleFoto'></div>"),
