861f9fd080e090c7bd1c81df873b54e8353199a7
mvarewyck <machteld.varewyck@openanalytics.eu>
2024-07-19 18:41:11 +0200

    fix #106 - duplicate columns after rework timeseries

--- alienSpecies/R/mapOccurrence.R
+++ alienSpecies/R/mapOccurrence.R
@@ -756,12 +756,7 @@ mapCubeServer <- function(id, uiText, species, gewest, df, shapeData,
         data = reactive({
             validate(need(gewest(), noData()))
             req(filterData())
-            if (!is.null(shapeData))
-              merge(filterData(), 
-                # attach regions for coloring
-                sf::st_drop_geometry(shapeData$utm1_bel_with_regions)[, c("CELLCODE", paste0("is", simpleCap(gewest())))], 
-                by.x = "cell_code1", by.y = "CELLCODE", all.x = TRUE) else
-              filterData()
+            filterData()
           }),
         period = reactive(input$period),
         combine = reactive(input$combine),
