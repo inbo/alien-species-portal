32476dfbfc7567ede7aefd2761e47e1630aeda5f
Saar Junius <saar.junius@openanalytics.eu>
2025-09-11 14:11:35 +0200

    feat #178 - numericInputs only update on blur

--- alienSpecies/R/mapOccurrence.R
+++ alienSpecies/R/mapOccurrence.R
@@ -507,21 +507,37 @@ mapCubeServer <- function(id, species, gewest, df, shapeData,
           
           periodChoice <- c(1950, currentYear)
           
-          div(style = "margin-left:50px; margin-right:10px;",
-            tagList(
-              sliderInput(
-                inputId = ns("period"), 
-                label = NULL,
-                min = 1950,
-                max = currentYear,
-                value = periodChoice,
-                sep = "", 
-                width = "100%"
-              ),
-              fluidRow(
-                column(2, numericInput(ns("periodStart"), translate("startYear")$title, value = 1950)),
-                column(2, offset = 8, numericInput(ns("periodEnd"), translate("endYear")$title, value = currentYear))
-              
+          tagList(
+            tags$script(sprintf("
+                  $(document).ready(function() {
+                  $('#%s').on('blur', function() {
+                  Shiny.setInputValue('%s', this.value, {priority: 'event'});
+                  });
+                  });
+                  ", ns("periodStart"), ns("periodStart_blur"))),
+            tags$script(sprintf("
+                  $(document).ready(function() {
+                  $('#%s').on('blur', function() {
+                  Shiny.setInputValue('%s', this.value, {priority: 'event'});
+                  });
+                  });
+                  ", ns("periodEnd"), ns("periodEnd_blur"))),
+            div(style = "margin-left:50px; margin-right:10px;",
+              tagList(
+                sliderInput(
+                  inputId = ns("period"), 
+                  label = NULL,
+                  min = 1950,
+                  max = currentYear,
+                  value = periodChoice,
+                  sep = "", 
+                  width = "100%"
+                ),
+                fluidRow(
+                  column(2, numericInput(ns("periodStart"), translate("startYear")$title, value = 1950)),
+                  column(2, offset = 8, numericInput(ns("periodEnd"), translate("endYear")$title, value = currentYear))
+                
+                )
               )
             )
           )
@@ -538,14 +554,14 @@ mapCubeServer <- function(id, species, gewest, df, shapeData,
           
         })
       
-      observeEvent(input$periodStart, priority = 5, {
+      observeEvent(input$periodStart_blur, priority = 5, {
           if (input$period[1] != input$periodStart) {
             updateSliderInput(session = session, inputId = "period", value = c(input$periodStart, input$period[2]))
           }
           
         })
       
-      observeEvent(input$periodEnd, priority = 5, {
+      observeEvent(input$periodEnd_blur, priority = 5, {
           if (input$period[2] != input$periodEnd) {
             updateSliderInput(session = session, inputId = "period", value = c(input$period[1], input$periodEnd))
           }
